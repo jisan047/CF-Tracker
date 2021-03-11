88869d16924990ae1bd73a7b8393ffa0f48b945a
Md. Jisan Shaikh <skjishan047@gmail.com>
2021-03-11 21:26:22 +0600

    #Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import TextField from "@material-ui/core/TextField";
+import Avatar from '@material-ui/core/Avatar';
 
 function App() {
+  const [userInfo, setUserInfo] = useState([]);
+
+  useEffect(() => {
+    console.log(userInfo.length > 0 ? userInfo[0].lastOnlineTimeSeconds > userInfo[0].registrationTimeSeconds: "null");
+  }, [userInfo]);
+
+  const fetchData = (e) => {
+    const userName = e.target.value;
+    const infoUrl = `http://codeforces.com/api/user.info?handles=${userName}`;
+    fetch(infoUrl)
+      .then((response) => response.json())
+      .then((result) => setUserInfo(result.result || []));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <header>
+          <h1>CF Tracker</h1>
+        </header>
+        <div className="search">
+          <TextField
+            className="search-field"
+            label="Username"
+            variant="outlined"
+            onChange={fetchData}
+          />
+        </div>
+        <div className="basic-info">
+          {userInfo.length > 0? (
+            <>
+              <div className="basic-info-pane">
+                <h3>Basic Information</h3>
+                <Avatar className="avatar" alt={`${userInfo[0].firstName} ${userInfo[0].lastName}`} src={userInfo[0].avatar}></Avatar>
+                <h5>{`Name: ${userInfo[0].firstName || ""} ${userInfo[0].lastName || ""}`}</h5>
+                <h5>{`City: ${userInfo[0].city || ""}`}</h5>
+                <h5>{`Country: ${userInfo[0].country || ""}`}</h5>
+              </div>
+              <div className="basic-info-pane"></div>
+              <div className="basic-info-pane"></div>
+            </>
+          ) : (
+            <></>
+          )}
+        </div>
+      </main>
     </div>
   );
 }
